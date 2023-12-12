f44f7ce0bace8548323c350e0ff7caa4c87e2c69
arthusagain <arthus.james@hotmail.com>
2023-12-12 17:00:06 -0300

    scroll animation bonus: dispose when scrolled past content

--- scroll-animation/scroll-animation.js
+++ scroll-animation/scroll-animation.js
@@ -5,12 +5,14 @@ window.addEventListener('scroll', checkBoxes);
 checkBoxes();
 
 function checkBoxes() {
-    const triggerBottom = window.innerHeight / 6 * 5;
+    const triggerTop = window.innerHeight / 6;
+    const triggerBottom = triggerTop * 5;
 
     boxes.forEach(box => {
         const boxTop = box.getBoundingClientRect().top;
+        const boxBottom = box.getBoundingClientRect().bottom;
 
-        if(boxTop < triggerBottom) {
+        if(boxTop < triggerBottom && boxBottom > triggerTop) {
             box.classList.add('content__container--show');
         } else {
             box.classList.remove('content__container--show');
